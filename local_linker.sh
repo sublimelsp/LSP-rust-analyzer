95e823a08b6f787d7f7502c26b1115b89d80585e
ms <platatita@gmail.com>
2021-12-30 12:25:56 +0100

    Improve the script to create the symbolic link of the project into sublime packages

--- local_linker.sh
+++ local_linker.sh
@@ -2,13 +2,21 @@
 
 # It creates the symbolic link from this source package to sublime package
 
-SUBLIME_PACKAGE="/Users/msz/Library/Application Support/Sublime Text 3/Packages/LSP-rust-analyzer"
+
+if [[ "$OSTYPE" == "linux-gnu" ]];
+then
+    SUBLIME_PACKAGE="$HOME/.config/sublime-text-3/Packages/LSP-rust-analyzer"
+else
+    SUBLIME_PACKAGE="$HOME/Library/Application Support/Sublime Text 3/Packages/LSP-rust-analyzer"
+fi
+
 SOURCE_PACKAGE=`pwd`
 
 if [ -L "$SUBLIME_PACKAGE" ];
 then
-    echo "Link '$SUBLIME_PACKAGE' exists"
-else
-    echo "Link '$SUBLIME_PACKAGE' does not exist. Will be created"
-    ln -s "$SOURCE_PACKAGE" "$SUBLIME_PACKAGE"
+    echo "Link '$SUBLIME_PACKAGE' exists. Will be unlinked"
+    unlink "$SUBLIME_PACKAGE"
 fi
+
+echo "Link '$SUBLIME_PACKAGE' will be created"
+ln -s "$SOURCE_PACKAGE" "$SUBLIME_PACKAGE"
