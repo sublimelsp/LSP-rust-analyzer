c1cf703ddbe453134e7b7540612b1831062aafe8
rchl <153197+rchl@users.noreply.github.com>
2025-08-25 07:31:30 +0000

    update rust-analyzer to 2025-08-25

--- plugin.py
+++ plugin.py
@@ -30,7 +30,7 @@ except ImportError:
 
 SESSION_NAME = "rust-analyzer"
 
-TAG = "2025-08-11"
+TAG = "2025-08-25"
 """
 Update this single git tag to download a newer version.
 After changing this tag, go through the server settings again to see
