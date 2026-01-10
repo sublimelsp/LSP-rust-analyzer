0f830eb4366024286335d8048717e9ba97de0b7e
Rafal Chlodnicki <rchl2k@gmail.com>
2026-01-10 10:13:53 +0100

    fix: invalid syntax in python 3.8

--- plugin.py
+++ plugin.py
@@ -465,8 +465,8 @@ class RustAnalyzerExpandMacro(RustAnalyzerCommand):
         window = self.view.window()
         if window is None:
             return
-        header = f"Recursive expansion of {expanded_macro["name"]}! macro"
-        content = f"// {header}\n// {(1 + len(header)) * "="}\n\n{expanded_macro["expansion"]}"
+        header = f"Recursive expansion of {expanded_macro['name']}! macro"
+        content = f"// {header}\n// {(1 + len(header)) * "="}\n\n{expanded_macro['expansion']}"
         sheets = window.selected_sheets()
         view = window.new_file(flags=sublime.TRANSIENT)
         view.set_scratch(True)
